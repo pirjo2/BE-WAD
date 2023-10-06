86fb277969005f025469428df2d71e4d21258213
KarlTomasson <karltomasson3@gmail.com>
2023-10-06 17:52:27 +0300

    lisasin javascripti login lehele

---
+++ script.js
@@ -0,0 +1,14 @@
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+const button = document.getElementById("lognupp");
+const section = document.getElementById("nupud");
+
+button.disabled = true;
+
+section.addEventListener('input', function() {
+    if (email.value == "" || pass.value == "") {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+})
